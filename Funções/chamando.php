8d1b4b260fb8914b9d368c56412b42f10d3f4c60
kyoleht <rafaelferreiradasilva1812@gmail.com>
2024-08-11 00:46:02 -0300

    chamando-funcao

---
+++ Funções/chamando.php
@@ -0,0 +1,18 @@
+<?php 
+
+        
+    // definindo uma função
+    function soma() {
+        $a = 5;
+        $b = 5;
+
+        echo $a + $b . "<br>";
+
+    }
+
+    // chamando 
+    soma();
+
+    //funcao php
+    echo("testando função");
+?>
